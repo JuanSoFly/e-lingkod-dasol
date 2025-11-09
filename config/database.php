e224731df2a4fc99dfd15de055289978dd0ec9bf
Juan Teodoro N. De Vera <juanteodoro.devera@gmail.com>
2025-11-09 15:46:36 +0800

    yes

--- config/database.php
+++ config/database.php
@@ -153,19 +153,19 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'shuttle.proxy.rlwy.net'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
+            'password' => env('REDIS_PASSWORD', 'RBLzgGDriUQCeJsaPlbxTdpryYmUxnOG'),
+            'port' => env('REDIS_PORT', '41198'),
             'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'shuttle.proxy.rlwy.net'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
+            'password' => env('REDIS_PASSWORD' , 'RBLzgGDriUQCeJsaPlbxTdpryYmUxnOG'),
+            'port' => env('REDIS_PORT', '41198'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
